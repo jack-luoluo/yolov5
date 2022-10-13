5f63b5375f5f01551a1914900eea63ce87265d19
Ruifeng Luo <lrf@tongji.edu.cn>
2022-10-13 13:51:59 +0800

    Create try.py

---
+++ try.py
@@ -0,0 +1,13 @@
+import torch
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
+
+# Images
+img = "D:\Pro\yolov5\下载.png"  # or file, Path, PIL, OpenCV, numpy, list
+
+# Inference
+results = model(img)
+
+# Results
+results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
